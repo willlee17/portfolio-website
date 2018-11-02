5b7c87a586ca0b911bbc6fd4ca3c5e77ec59f594
William Lee <willlee@sas.upenn.edu>
2018-11-01 22:43:49 -0700

    Changed pictures, made text more responsive

--- js/script.js
+++ js/script.js
@@ -1,28 +1,7 @@
-// $(document).ready(function() {
-//   $('h1').hide(1000).show(1000).slideUp(1000).slideDown(1000);
-//
-//   $('#main').css({
-//     color: 'red',
-//     fontSize: 40,
-//   })
-//
-//   $('h1').click(function() {
-//     $('#main').css({
-//       color: 'blue',
-//       fontSize: 80,
-//     })
-//   })
-// })
-//
-// $(window).on("load", function() {
-//   alert("Window has loaded")
-// })
-
-
 // Preloader
 $(window).on('load', function() {
   $('#status').fadeOut()
-  $('#preloader').delay(350).fadeOut('slow')
+  $('#preloader').delay(550).fadeOut('slow')
 })
 
 // Projects
